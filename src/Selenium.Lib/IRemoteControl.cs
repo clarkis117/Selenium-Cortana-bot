92f3d1215d6fa7fc42ae5ebacccdbb52eac139bd
Joshua Clark <clarkis117@live.com>
2017-03-01 07:26:33 -0500

    Add project files.

---
+++ src/Selenium.Lib/IRemoteControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Selenium.Lib
+{
+	public enum RequestState : byte { Success, Blocked, Failed };
+
+	public interface IRemoteControl
+	{
+		Task<bool> IsBrowserServiceUp();
+
+		Task<RequestState> OpenBrowser();
+
+		Task<RequestState> CloseBrowser();
+
+		Task<RequestState> Forward();
+
+		Task<RequestState> Back();
+
+		Task<RequestState> Search(string term);
+
+		Task SelectResult(uint count);
+
+		Task SelectResult(string title);
+
+		Task SelectResult(string website, uint count = 1);
+	}
+}
---
+++ src/Selenium/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Selenium
+{
+	public class Program
+	{
+		public const string ExeLoc = "./";
+
+		public static void Main(string[] args)
+		{
+			EdgeOptions opts = new EdgeOptions();
+
+			EdgeDriverService ser = EdgeDriverService.CreateDefaultService(ExeLoc);
+
+			IWebDriver driver = new EdgeDriver(ser);
+
+			driver.Url = "http://bing.com";
+
+			var element = driver.FindElement(By.Id("sb_form_q"));
+
+			element.SendKeys("What is a cortana skill? in C# \n\r");
+		}
+	}
+}
